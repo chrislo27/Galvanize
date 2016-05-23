e5768d358b4fee7db8db08837267413d26d9de37
chrislo27 <kinchrislo@gmail.com>
2016-05-22 20:00:52 -0700

    Added standard keybinds

---
+++ core/src/chrislo27/galvanize/Keybinds.java
@@ -0,0 +1,12 @@
+package chrislo27.galvanize;
+
+import com.badlogic.gdx.Input.Keys;
+
+public class Keybinds {
+
+	public static final int[] UP = { Keys.W, Keys.UP };
+	public static final int[] DOWN = { Keys.S, Keys.DOWN };
+	public static final int[] LEFT = { Keys.A, Keys.LEFT };
+	public static final int[] RIGHT = { Keys.D, Keys.RIGHT };
+
+}
